db7b7f75957f2f24b59fc8168523742579964a2f
fatih <fatih.tutar@dijitalvarliklar.com.tr>
2022-11-02 15:44:38 +0300

    News Card

--- app/components/BlockCard.js
+++ app/components/BlockCard.js
@@ -1,16 +1,39 @@
 import React from "react";
 import { View, StyleSheet, Image } from 'react-native';
+import Title from "./Title";
+import Subtitle from "./Subtitle";
 
 const BlockCard = () => {
     return (
         <View style={styles.container}>
-            <Image source={require('../../assets/erdoganintroo.jpeg')} />
+            <Image source={require('../../assets/erdoganintroo.jpeg')} style={styles.image}/>
+            <View style={styles.contentContainer}>
+                <Title>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto pariatur dolores ab natus neque doloremque maiores. Nulla, impedit quo eius, earum minima quam aliquam aperiam voluptas temporibus laborum deleniti distinctio!
+                </Title>
+                <Subtitle>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto pariatur dolores ab natus neque doloremque maiores. Nulla, impedit quo eius, earum minima quam aliquam aperiam voluptas temporibus laborum deleniti distinctio!
+                </Subtitle>
+            </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    container:{}
+    container: {
+        width: '100%',
+        height: 300,
+        borderRadius: 8,
+        overflow: 'hidden',
+        backgroundColor: '#fff'
+    },
+    image: {
+        width: '100%',
+        height: 200
+    },
+    contentContainer: {
+        padding: 5
+    }
 })
 
 export default BlockCard
--- app/components/Title.js
+++ app/components/Title.js
@@ -1,25 +1,10 @@
-//import liraries
-import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React from 'react';
+import { Text } from 'react-native';
 
-// create a component
-const Title = () => {
+const Title = ({children, numberOfLines = 2, size = 18}) => {
     return (
-        <View style={styles.container}>
-            <Text>Title</Text>
-        </View>
+        <Text numberOfLines={numberOfLines} style={{fontWeight: 'bold', fontSize: size}}>{children}</Text>
     );
 };
 
-// define your styles
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#2c3e50',
-    },
-});
-
-//make this component available to the app
 export default Title;
