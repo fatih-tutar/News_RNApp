0c44abe6b045631e10facfaad19e6dce84d55c15
fatih <fatih.tutar@dijitalvarliklar.com.tr>
2022-11-17 10:33:20 +0300

    Connecting to Backend

--- App.js
+++ App.js
@@ -1,39 +1,30 @@
-import SearchBar from './app/components/SearchBar';
-import Screen from './app/components/Screen';
-import FeaturedNews from './app/components/FeaturedNews';
-import SmallCard from './app/components/SmallCard';
-import BreakingNews from './app/components/BreakingNews';
+import React, { useState, useEffect } from "react";
 
-import data from './fakeData';
-import TechNews from './app/components/TechNews';
-import FlatCard from './app/components/FlatCard';
-import PoliticalNews from './app/components/PoliticalNews';
-import EntertainmentNews from './app/components/EntertainmentNews';
+import Screen from './app/components/Screen';
+import { Text } from 'react-native';
 
 export default function App() {
+  const [num, setNum] = useState(0);
 
-  const breakingNews = data.filter(item => item.category === 'breaking-news');
-  const techNews = data.filter(item => item.category === 'tech');
-  const politicalNews = data.filter(item => item.category === 'political');
-  const entertainmentNews = data.filter(item => item.category === 'entertainment');
+  const useState = (initialValue) => {
+    let state = initialValue;
+
+    function functionToResetState (value) {
+      state = value
+    }
+
+    return [state, functionToResetState]
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      setNum(num + 1)
+    }, 1000);
+  }, [stateone, statetwo]);
 
   return (
     <Screen>
-      <SearchBar/>
-      <FeaturedNews 
-        item={{
-          id: "8",
-          title: "This is the title no eight.",
-          desc:
-            "Desc is the short form of description and this format is the actual same as our real database.",
-          thumbnail: "https://picsum.photos/400/200",
-          category: "tech"
-        }}
-      />
-      <BreakingNews data={breakingNews} />
-      <PoliticalNews data={politicalNews} />
-      <TechNews data={techNews} />
-      <EntertainmentNews data={entertainmentNews} />
+      <Text>The value of num is {num}</Text>
     </Screen>
-  );
+  )
 }
