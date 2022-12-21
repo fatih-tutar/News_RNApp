1eb834558952415cbcb28afa3a1ab52c0f663577
fatih <fatih.tutar@dijitalvarliklar.com.tr>
2022-12-21 15:30:27 +0300

    Actual use of useState & useEffect

--- App.js
+++ App.js
@@ -1,30 +1,33 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
+
+import useNews from './app/hooks/useNews';
 
 import Screen from './app/components/Screen';
-import { Text } from 'react-native';
+import SearchBar from './app/components/SearchBar';
+import FeaturedNews from './app/components/FeaturedNews';
+import BreakingNews from './app/components/BreakingNews';
+import PoliticalNews from './app/components/PoliticalNews';
+import TechNews from './app/components/TechNews';
+import EntertainmentNews from './app/components/EntertainmentNews';
 
 export default function App() {
-  const [num, setNum] = useState(0);
-
-  const useState = (initialValue) => {
-    let state = initialValue;
-
-    function functionToResetState (value) {
-      state = value
-    }
-
-    return [state, functionToResetState]
-  }
-
-  useEffect(() => {
-    setTimeout(() => {
-      setNum(num + 1)
-    }, 1000);
-  }, [stateone, statetwo]);
+  
+  const [
+    featuredNews, 
+    politicalNews, 
+    entertainmentNews, 
+    techNews, 
+    breakingNews
+  ] = useNews();
 
   return (
     <Screen>
-      <Text>The value of num is {num}</Text>
+      <SearchBar/>
+      <FeaturedNews item={featuredNews}/>
+      <BreakingNews data={breakingNews}/>
+      <PoliticalNews data={politicalNews}/>
+      <TechNews data={techNews}/>
+      <EntertainmentNews data={entertainmentNews}/>
     </Screen>
   )
 }
--- app/api/client.js
+++ app/api/client.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'http://192.168.28.50:3000/api'
+    baseURL: 'http://localhost:3000/api'
 });
